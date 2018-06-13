27c3d8a3b9682675b368a0e6a2be9ce76d1e2132
Matt <legendblade@users.noreply.github.com>
2018-06-13 06:40:12 -0700

    Cleaning some things up.

--- src/main/java/org/winterblade/thaumicstages/handlers/DustStages.java
+++ src/main/java/org/winterblade/thaumicstages/handlers/DustStages.java
@@ -13,15 +13,29 @@ import stanhebben.zenscript.annotations.ZenMethod;
 
 @ModOnly("thaumcraft")
 @ZenRegister
-@ZenClass("mods.thaumcraft.stages.DustStages")
+@ZenClass("mods.thaumcraft.Dust")
 public class DustStages {
+    /**
+     * Adds a stage to the given dust block transform
+     * @param stage The stage to add
+     * @param block The block to check for
+     * @param error The error message to give, if any
+     */
     @ZenMethod
     public static void addStage(String stage, IItemStack block, @Optional String error) {
         CraftTweakerAPI.apply(new AddSimpleDustStageAction(stage, block, error));
     }
 
+    /**
+     * Adds a stage to the given dust ore dictionary transform
+     * @param stage The stage to add
+     * @param ore   The ore dict entry to add it to
+     * @param error The error message to give, if any
+     */
     @ZenMethod
     public static void addStage(String stage, IOreDictEntry ore, @Optional String error) {
         CraftTweakerAPI.apply(new AddOreDustStageAction(stage, ore, error));
     }
+
+    
 }
